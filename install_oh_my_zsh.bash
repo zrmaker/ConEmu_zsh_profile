aeec4e7b96376617bd604664d4cce9c5590aa50c
Renyuan Zhang <yuanisbug@gmail.com>
2018-03-26 19:34:30 -0700

    normalized endings

--- install_oh_my_zsh.bash
+++ install_oh_my_zsh.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-sudo apt get update
+sudo apt update
 sudo apt install zsh
 zsh version
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 chsh -s /bin/zsh
+
+sudo apt install fonts-powerline
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 cp -f .zshrc ~/.zshrc
