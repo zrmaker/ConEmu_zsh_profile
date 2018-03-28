b6d2a965b2206101208c5f107d9c47c7d1cfceae
Leo Zhang <yuanisbug@gmail.com>
2018-03-27 21:37:08 -0700

    add background

--- install.sh
+++ install.sh
@@ -12,4 +12,5 @@ sudo apt install fonts-powerline
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-cp -f .zshrc ~/.zshrc
+cp -f ./OneDrive/ConEmu/.zshrc ~/.zshrc
+source ~/.zshrc
