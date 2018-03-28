0312a805eb58324f0355a4a690b2a063c1951aec
Leo Zhang <yuanisbug@gmail.com>
2018-03-28 14:41:15 -0700

    minor changes

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ chsh -s /bin/zsh
 
 sudo apt install fonts-powerline
 
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
